d79b5f51c906745ab539565c42c97b5a9593e783
Abhay Singh <141265413+abhay9876@users.noreply.github.com>
2023-08-04 12:14:13 +0530

    Create While_loop.java

---
+++ While_loop.java
@@ -0,0 +1,12 @@
+public class While_loop {
+    public static void main(String[]args){
+        int n=10 ,i=1;
+        while(i<=n){
+            System.out.println(i);
+            ++i;
+            
+        }
+    }
+
+    
+}
