1fcb0297105fb1e17ad2772e3225daacec3ab22c
Abhay Singh <141265413+abhay9876@users.noreply.github.com>
2023-08-04 11:29:02 +0530

    Create Area_of_circle.java

---
+++ Area_of_circle.java
@@ -0,0 +1,9 @@
+class Area_of_circle {
+    public static void main(String[] arg) {
+        int r = 5;
+        double pi = 3.14;
+        double area = pi * r * r;
+        System.out.println("Area of circle is : "+ area);
+    }
+
+}
