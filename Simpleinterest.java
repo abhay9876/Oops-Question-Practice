85eafc4d09d8293ed618e8aa0c78100161a2d105
Abhay Singh <141265413+abhay9876@users.noreply.github.com>
2023-08-04 11:21:44 +0530

    Create Simpleinterest.java

---
+++ Simpleinterest.java
@@ -0,0 +1,10 @@
+public class Simpleinterest {
+    public static void main(String[] arg) {
+        int p = 5;
+        int r = 7;
+        int t = 5;
+        double si = (p * r * t) / 100.0f;
+        System.out.println(si);
+    }
+
+}
