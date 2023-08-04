31c317abd49d124c161f21d6ad4e9496ef82e048
Abhay Singh <141265413+abhay9876@users.noreply.github.com>
2023-08-04 11:34:55 +0530

    Create Area_of_rectangle.java

---
+++ Area_of_rectangle.java
@@ -0,0 +1,9 @@
+public class Area_of_rectangle {
+    public static void main(String[] arg) {
+        int l = 5;
+        int w = 7;
+        double area = l*w;
+        System.out.println("Area of rectangle is : "+area);
+    }
+
+}
