46ee222198f740c91398d7a845e74d456d316759
Abhay Singh <141265413+abhay9876@users.noreply.github.com>
2023-08-04 11:15:03 +0530

    Create Test.java

---
+++ Test.java
@@ -0,0 +1,7 @@
+class Test {
+    public static void main(String[] args) {
+        System.out.println("JAI GLA");
+        System.out.println("Abhay Singh");
+        System.out.println("2215000023");
+    }
+}
